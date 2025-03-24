4f6bb4c3bd68425b8e410569dc0a4388f9612ec5
abelbm02 <abelblanco447@gmail.com>
2025-03-24 11:21:24 +0100

    aa

---
+++ Lenguajes/EJ_JavaScript/005paginaSimple.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Variables
+let username;
+
+// Pedir nombre por teclado
+prompt("Introduce tu nombre: ", username);
+
+// Mostrar nombre en la página
+console.log("Hola " + username);
