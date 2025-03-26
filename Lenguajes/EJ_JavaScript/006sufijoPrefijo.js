6ad36aba1f3175704568564dc13d0bf5ea6fa48b
Abel Blanco Muñoz <abelblanco447@gmail.com>
2025-03-26 12:02:58 +0100

    actv6

---
+++ Lenguajes/EJ_JavaScript/006sufijoPrefijo.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Asignar valores a las variables a y b
+let a = 1, b = 1;
+
+// Con prefijo, primero se incrementa a y luego se asigna a c
+let c = ++a; // ?
+
+// Con sufijo primero se asigna b a d y luego se incrementa b
+let d = b++; // ?
