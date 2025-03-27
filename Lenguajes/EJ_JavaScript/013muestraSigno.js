3571c3dd2220022c937940a8398c92f72a11793c
Abel Blanco Muñoz <abelblanco447@gmail.com>
2025-03-27 10:49:09 +0100

    13

--- Lenguajes/EJ_JavaScript/013muestraSigno.js
+++ Lenguajes/EJ_JavaScript/013muestraSigno.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// Declaramos la variable nume y le asignamos el valor introducido por el usuario
+let nume = prompt("Introduce un número: ");
+
+// Comprobamos si el número es mayor, menor o igual a 0
+if (nume > 0) {
+    alert("1");
+} else if (nume < 0) {
+    alert("-1");
+} else if (nume == 0) {
+    alert("0");
+}
