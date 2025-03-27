93e9adb9b2d0dec9c1e110f2a70f9e994a734c89
Abel Blanco Muñoz <abelblanco447@gmail.com>
2025-03-27 11:20:12 +0100

    17

---
+++ Lenguajes/EJ_JavaScript/017compruebaRangoInterno.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let edad = prompt("Introduce tu edad: ");
+
+// Con el => podemos hacer que se nincluyan los extermos del rango
+if (edad >= 18 && edad <= 99) {
+    alert("Estas haciendote mayor")
+}
+else { alert("Eres un chaval") }12
