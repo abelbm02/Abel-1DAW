4387adff319ba85a2c66fb3f92cf055469eaad7c
abelbm02 <abelblanco447@gmail.com>
2025-04-07 12:29:17 +0200

    Cambios

--- Lenguajes/PensamientoLogJS/003 chooseVegetarian.js
+++ Lenguajes/PensamientoLogJS/003 chooseVegetarian.js
@@ -3,3 +3,24 @@
 let comida = prompt ("Quiere cenar, almorzar o desayunar?");
 
 let veg = prompt ("Es vegetariano? (si/no)");
+
+if (comida == "cena " && veg == "si") {
+    console.log ("Ensalada de lechuga y cebolla");
+}
+else if (comida == "cena" && veg == "no") {
+    console.log ("Carne asada con papas fritas");
+}
+
+if (comida == "almuerzo" && veg == "si") {
+    console.log ("Lentejas con arroz");
+}   
+else if (comida == "almuerzo" && veg == "no") {
+    console.log ("Pollo con verduras al vapor")
+};
+
+if (comida == "desayuno" && veg == "si") {
+    console.log ("Tostadas con aguacate");
+}
+else if (comida == "desayuno" && veg == "no") {
+    console.log ("Tostada de jamón y queso")
+}
